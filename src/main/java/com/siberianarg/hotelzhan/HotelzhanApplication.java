5b6d1965e6ad44a917417f799921cb311c8198f4
Johnny <galim999@mail.ru>
2022-10-04 13:43:35 +0600

    build gradle

---
+++ src/main/java/com/siberianarg/hotelzhan/HotelzhanApplication.java
@@ -0,0 +1,13 @@
+package com.siberianarg.hotelzhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelzhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelzhanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siberianarg/hotelzhan/HotelzhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siberianarg.hotelzhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelzhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
